0cc9ec5527b4d643ae14f5a1dcee6acecb6630f8
Brent Groves <brent@robogroves.com>
2016-01-06 18:30:10 -0500

    Initial commit

---
+++ src/main/java/com/robogroves/pi4led/com/robogroves/pi4led/controller/LedController.java
@@ -0,0 +1,25 @@
+package com.robogroves.pi4led.com.robogroves.pi4led.controller;
+
+
+import com.pi4j.io.gpio.*;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+public class LedController {
+
+    private static GpioPinDigitalOutput pin;
+    @RequestMapping("/")
+    public String greeting(){
+        return "Hello world!";
+    }
+
+    @RequestMapping("/light")
+    public String light(){
+        if(pin==null){
+            GpioController gpio = GpioFactory.getInstance();
+            pin = gpio.provisionDigitalOutputPin(RaspiPin.GPIO_02,"MyLED", PinState.LOW);
+        }
+        pin.toggle();
+
+        return "OK";
+    }
+}
