4fe95a6b318987ca731e09e1e13bc302c5302a1c
Brent Groves <brent@robogroves.com>
2016-01-06 19:54:13 -0500

    Oops2

--- src/main/java/com/robogroves/pi4led/com/robogroves/pi4led/controller/LedController.java
+++ src/main/java/com/robogroves/pi4led/com/robogroves/pi4led/controller/LedController.java
@@ -18,7 +18,7 @@ public class LedController {
     public String light(){
         if(pin==null){
             GpioController gpio = GpioFactory.getInstance();
-            pin = gpio.provisionDigitalOutputPin(RaspiPin.GPIO_02,"MyLED", PinState.LOW);
+            pin = gpio.provisionDigitalOutputPin(RaspiPin.GPIO_01,"MyLED", PinState.LOW);
         }
         pin.toggle();
 
